189970d9f6e303008edd329132faa27cd61df00f
Karl Bennett <karl@karl-youthnet.(none)>
2010-11-01 18:58:52 +0000

    Added the initialisation of the spring environment to the main class.

--- src/main/java/org/bails/Bails.java
+++ src/main/java/org/bails/Bails.java
@@ -1,5 +1,8 @@
 package org.bails;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * This is the first step to getting Bails up and running. The main class that will start up a bear bones spring
  * environment with Jetty to test the Bails templates and view resolver.
@@ -8,6 +11,7 @@ package org.bails;
 public class Bails {
 
     public static void main(String[] args) {
-
+       AbstractApplicationContext context = new ClassPathXmlApplicationContext("start-up.xml");
+       context.registerShutdownHook();
     }
 }
