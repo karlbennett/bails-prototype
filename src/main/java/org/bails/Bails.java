9a3604a7a7456736eb5c747b3591db37bc555c76
Karl Bennett <karl@karl-youthnet.(none)>
2010-11-01 18:30:17 +0000

    The first commit for the Bails project.

---
+++ src/main/java/org/bails/Bails.java
@@ -0,0 +1,13 @@
+package org.bails;
+
+/**
+ * This is the first step to getting Bails up and running. The main class that will start up a bear bones spring
+ * environment with Jetty to test the Bails templates and view resolver.
+ * @author Karl Bennett
+ */
+public class Bails {
+
+    public static void main(String[] args) {
+
+    }
+}
