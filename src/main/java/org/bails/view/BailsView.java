79d49d8697203be552aad4ea19cc4d5249027abd
Karl Bennett <karl@karl-youthnet.(none)>
2010-11-02 17:20:22 +0000

    Added a getter and setter for a url string to the BailsView. This is then automatically populated by Spring with the url for the template file returned by the controllers request method.

--- src/main/java/org/bails/view/BailsView.java
+++ src/main/java/org/bails/view/BailsView.java
@@ -10,9 +10,28 @@ import java.util.Map;
  * @author Karl Bennett
  */
 public class BailsView extends AbstractTemplateView {
+
+    private String url;
+
+    /**
+	 * Set the URL of the resource that this view wraps.
+	 * The URL must be appropriate for the concrete View implementation.
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/**
+	 * Return the URL of the resource that this view wraps.
+	 */
+	public String getUrl() {
+		return this.url;
+	}
+
     @Override
     protected void renderMergedTemplateModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        response.getWriter().write("Hello Bails!");
+        response.getWriter().write("Hello Bails!<br/>");
+        response.getWriter().write(getUrl());
         response.getWriter().flush();
         response.getWriter().close();
     }
