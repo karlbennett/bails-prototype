6ff4f11ca67450562c7327076fd1b5c6512b6128
Karl Bennett <karl@karl-youthnet.(none)>
2010-11-12 15:02:34 +0000

    Commented the IBailsStream interface.

--- src/main/java/org/bails/IBailsStream.java
+++ src/main/java/org/bails/IBailsStream.java
@@ -9,23 +9,54 @@ import java.util.Map;
  */
 public interface IBailsStream {
 
+    /**
+     * @return true if the stream has more elements to process.
+     */
     public boolean hasNext();
 
+    /**
+     * Move to the next element in the stream populating all the flags and attributes appropriately.
+     */
     public void next();
 
+    /**
+     * Close the stream freeing up and related resources.
+     */
     public void close();
 
+    /**
+     * @return true if the current element is an opening tag.
+     */
     public boolean isOpenTag();
 
+    /**
+     * @return true if the current element is an closing tag.
+     */
     public boolean isCloseTag();
 
+    /**
+     * @return true if the current element is an open close tag.
+     */
     public boolean isOpenCloseTag();
 
+    /**
+     * @return true if the current element is a characters element.
+     */
     public boolean isCharacters();
 
+    /**
+     * @return the character sequence representation of the current element. This should preserve all surrounding
+     * whitespace.
+     */
     public CharSequence getCharSequence();
 
+    /**
+     * @return the name of the current element if it contains a name other wise null.
+     */
     public String getName();
 
+    /**
+     * @return the attributes related to the current element if it contains attributes otherwise null.
+     */
     public Map<String, Object> getAttributes();
 }
