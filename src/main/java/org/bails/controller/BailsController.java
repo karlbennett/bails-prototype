f646aeca8d1c9bb469e0e06c6162b03e5900dc3a
Karl Bennett <karl@karl-youthnet.(none)>
2010-11-14 23:18:07 +0000

    Updated the prototype controller so that it now passes in a model object.

--- src/main/java/org/bails/controller/BailsController.java
+++ src/main/java/org/bails/controller/BailsController.java
@@ -1,6 +1,7 @@
 package org.bails.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BailsController {
 
+    @ModelAttribute("message")
+    public String getMessage() {
+        return "This text is coming from the controller right to your page.";
+    }
+
     @RequestMapping("/index.html")
-    public String index() {
+    public String index(@ModelAttribute("message") String message) {
         return "PrototypePage";
     }
 }
