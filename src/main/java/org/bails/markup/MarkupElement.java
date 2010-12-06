59576d258b431cf201890f5ac454f91d49413d8c
Karl Bennett <pyr0@Max.(none)>
2010-12-06 00:16:54 +0000

    Added ancestor, bailsMarkupChildren, and bailsChildren attributes to the MarkupElement class.
    ancestor: Holds the top element in the markup element tree.
    bailsMarkupChildren: Holds a map of all BailsTagElements that were added from the markup document.
    bailsChildren: Holds a map of all the BailsTagElements that were added from within a Page object.

--- src/main/java/org/bails/markup/MarkupElement.java
+++ src/main/java/org/bails/markup/MarkupElement.java
@@ -3,9 +3,7 @@ package org.bails.markup;
 import org.bails.stream.ELEMENT_TYPE;
 import org.bails.stream.IBailsStream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * This class gives an object representation of a tag within bails markup.
@@ -16,8 +14,11 @@ public class MarkupElement {
 
     public static final String BAILS_ID_NAME = "bails:id"; // Bails attribute name. Quick dirty and going to change.
 
+    private MarkupElement ancestor = this;
     private MarkupElement parent;
     private List children = new ArrayList();
+    private Map<String, BailsTagElement> bailsMarkupChildren = new HashMap<String, BailsTagElement>();
+    private Map<String, BailsTagElement> bailsChildren = new HashMap<String, BailsTagElement>();
 
     public MarkupElement() {
     }
@@ -41,36 +42,31 @@ public class MarkupElement {
     protected void addChildren(IBailsStream stream) {
         stream.next(); // Get the next element.
 
-        MarkupElement child = null;
         // Process any child elements until we reach the closing tag for this element.
         while (stream.hasNext() && stream.getType() != ELEMENT_TYPE.CLOSE) {
 
             switch (stream.getType()) {
                 case OPEN: {
-                    child = new TagElement(stream, stream.getCharSequence(), stream.getName(), stream.getAttributes());
-                    child.setParent(this);
+                    this.add(new TagElement(this, stream, stream.getCharSequence(), stream.getName(),
+                            stream.getAttributes()));
                     break;
                 }
                 case OPENCLOSE: {
-                    child = new TagElement(stream, stream.getCharSequence(), stream.getName(), stream.getAttributes(),
-                            true);
-                    child.setParent(this);
+                    this.add(new TagElement(this, stream, stream.getCharSequence(), stream.getName(),
+                            stream.getAttributes(),
+                            true));
                     break;
                 }
                 case CHARACTERS: {
-                    child = new CharactersElement(stream);
-                    child.setParent(this);
+                    this.add(new CharactersElement(this, stream));
                     break;
                 }
                 case BAILS: {
-                    child = new BailsTagElement(stream, stream.getBailsId(), stream.getCharSequence(), stream.getName(),
-                            stream.getAttributes());
-                    child.setParent(this);
+                    this.add(new BailsTagElement(this, stream, stream.getBailsId(), stream.getCharSequence(),
+                            stream.getName(), stream.getAttributes()));
                     break;
                 }
             }
-
-            if (child != null) this.children.add(child); // Add to this elements children.
 
             stream.next(); // Move to the next element.
         }
@@ -79,7 +75,6 @@ public class MarkupElement {
     public MarkupElement add(Object... childs) {
         for (Object child : childs) {
             children.add(child);
-            if (child instanceof MarkupElement) ((MarkupElement) child).setParent(this);
         }
 
         return this;
@@ -95,15 +90,74 @@ public class MarkupElement {
         return children == null ? null : children.get(i);
     }
 
+    /**
+     * Get a bails markup child from the MarkupElement using the bails path.
+     *
+     * @param path the path to the BailsTagElement. This is a string of bails ids separated by  colons (:).
+     * @return the child at the given path. Or null if the path is incorrect.
+     */
+    public BailsTagElement getBailsMarkupChild(String path) {
+        return bailsMarkupChildren.get(path);
+    }
+
+    /**
+     * Add a bails markup child to this element.
+     *
+     * @param path       the path of the bails child.
+     * @param bailsChild the child bails markup element.
+     * @return this element to allow for chaining.
+     */
+    public MarkupElement addBailsMarkupChild(String path, BailsTagElement bailsChild) {
+        bailsMarkupChildren.put(path, bailsChild);
+
+        return this;
+    }
+
+    /**
+     * Get a bails child from the MarkupElement using the bails path.
+     *
+     * @param path the path to the BailsTagElement. This is a string of bails ids separated by  colons (:).
+     * @return the child at the given path. Or null if the path is incorrect.
+     */
+    public BailsTagElement getBailsChild(String path) {
+        return bailsChildren.get(path);
+    }
+
+    /**
+     * Add a bails child to this element.
+     *
+     * @param path       the path of the bails child.
+     * @param bailsChild the child bails element.
+     * @return this element to allow for chaining.
+     */
+    public MarkupElement addBailsChild(String path, BailsTagElement bailsChild) {
+        bailsChildren.put(path, bailsChild);
+
+        return this;
+    }
+
+    public void setHeritage(MarkupElement parent) {
+        setAncestor(parent.getAncestor());
+        setParent(parent);
+    }
+
     /*
         Getters and Setters.
      */
+
+    public MarkupElement getAncestor() {
+        return ancestor;
+    }
+
+    private void setAncestor(MarkupElement ancestor) {
+        this.ancestor = ancestor;
+    }
 
     public MarkupElement getParent() {
         return parent;
     }
 
-    protected void setParent(MarkupElement parent) {
+    private void setParent(MarkupElement parent) {
         this.parent = parent;
     }
 
