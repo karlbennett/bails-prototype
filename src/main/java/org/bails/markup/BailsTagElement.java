c0db9f1a20af398bdc4fe3e55d74c34bb6f8bc1e
Karl Bennett <pyr0@Max.(none)>
2010-12-19 22:18:58 +0000

    Updated the BailsTagElement to implement the IBailsElement interface. It also now uses the buildBailsPath method from the BailsUtil class to build it's bails path when it is first requested.

--- src/main/java/org/bails/markup/BailsTagElement.java
+++ src/main/java/org/bails/markup/BailsTagElement.java
@@ -1,17 +1,21 @@
 package org.bails.markup;
 
 import org.bails.Element;
+import org.bails.IBailsElement;
 import org.bails.stream.IBailsStream;
 
 import java.util.Map;
+
+import static org.bails.util.BailsUtil.buildBailsPath;
 
 /**
  * User: Karl Bennett
  * Date: 14/12/10
  */
-public class BailsTagElement extends TagElement {
+public class BailsTagElement extends TagElement implements IBailsElement {
 
     private String bailsId;
+    private String bailsPath;
 
     public BailsTagElement(Element parent, IBailsStream stream, String bailsId, CharSequence openTag, String name,
                            Map<String, Object> attributes) {
@@ -24,11 +28,25 @@ public class BailsTagElement extends TagElement {
         this.bailsId = bailsId;
     }
 
+    @Override
     public String getBailsId() {
         return bailsId;
     }
 
     protected void setBailsId(String bailsId) {
         this.bailsId = bailsId;
+    }
+
+    @Override
+    public String getBailsPath() {
+        if (bailsPath == null) {
+            bailsPath = buildBailsPath(this);
+        }
+
+        return bailsPath;
+    }
+
+    protected void setBailsPath(String bailsPath) {
+        this.bailsPath = bailsPath;
     }
 }
