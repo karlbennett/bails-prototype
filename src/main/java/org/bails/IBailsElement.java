1d645cb944ec642733708f83b193c21b17a1978f
Karl Bennett <pyr0@Max.(none)>
2010-12-19 22:14:36 +0000

    Added IBailsElement so that the bails element functionality can be shared horizontally across class hierarchies.

---
+++ src/main/java/org/bails/IBailsElement.java
@@ -0,0 +1,12 @@
+package org.bails;
+
+/**
+ * User: Karl Bennett
+ * Date: 19/12/10
+ */
+public interface IBailsElement {
+
+    public String getBailsId();
+
+    public String getBailsPath();
+}
