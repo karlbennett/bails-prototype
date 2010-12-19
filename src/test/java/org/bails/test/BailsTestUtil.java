bf2ecef4b15db823627e341e45138dc4fe7facaf
Karl Bennett <pyr0@Max.(none)>
2010-12-19 22:15:58 +0000

    Updated the BailsTestUtil class to have an XML document with a nested bails tag that is not directly under another bails tag.

--- src/test/java/org/bails/test/BailsTestUtil.java
+++ src/test/java/org/bails/test/BailsTestUtil.java
@@ -8,13 +8,14 @@ import java.util.*;
  * @author Karl Bennett
  */
 @Ignore // Make sure this isn't mistaken for a unit test class.
-public final class TestBailsTestUtil {
+public final class BailsTestUtil {
 
-    private TestBailsTestUtil() {
+    private BailsTestUtil() {
     }
 
     public static final String TEST_BAILS_ID_ONE = "test_element_one";
     public static final String TEST_BAILS_ID_TWO = "test_element_two";
+    public static final String TEST_BAILS_ID_THREE = "test_element_three";
 
     public static final String XML_LINE_ONE =      "<element xmlns:bails='http://www.bails.org/' bails:id='" + TEST_BAILS_ID_ONE + "'>\n";
     public static final String XML_LINE_TWO =      "    <element one='1' two='2' three='3'>Some text one.</element>\n";
@@ -22,12 +23,13 @@ public final class TestBailsTestUtil {
     public static final String XML_LINE_FOUR =     "    <element bails:id='" + TEST_BAILS_ID_TWO + "'>Some text three.</element>\n";
     public static final String XML_LINE_FIVE =     "    <element></element>\n";
     public static final String XML_LINE_SIX =      "    <element/>\n";
-    public static final String XML_LINE_SEVEN =    "</element>";
+    public static final String XML_LINE_SEVEN =    "    <element><element bails:id='" + TEST_BAILS_ID_THREE + "'>Some text four.</element></element>\n";
+    public static final String XML_LINE_EIGHT =    "</element>";
 
     public static final String XML_DCOUMENT = XML_LINE_ONE + XML_LINE_TWO + XML_LINE_THREE + XML_LINE_FOUR
-            + XML_LINE_FIVE + XML_LINE_SIX + XML_LINE_SEVEN;
+            + XML_LINE_FIVE + XML_LINE_SIX + XML_LINE_SEVEN + XML_LINE_EIGHT;
 
-    public static final int CHILD_NUM = 5;
+    public static final int CHILD_NUM = 6;
 
     public static final String TEST_OPEN_TAG = "<element>\n";
     public static final String TEST_OPEN_CLOSE_TAG = "<element/>\n";
